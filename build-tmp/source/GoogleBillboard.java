6384e3c8da1c6e960ec31f0686e52d4c0e99f57d
charzan <stalkerbob123@gmail.com>
2016-01-23 13:05:04 -0800

    runs in the setup

--- build-tmp/source/GoogleBillboard.java
+++ build-tmp/source/GoogleBillboard.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class GoogleBillboard extends PApplet {
+
 public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
 public void setup()  
 {            
@@ -45,3 +61,12 @@ public boolean isPrime(double dNum)
 // 7. Add commit and push your finished program to your github account. Note that there is no web page for this assignment, so you will be pushing to the master branch, not gh-pages. 
 // 8. Submit the url of your GitHub repository via the school loop drop box for the assignment. The url will have the form http://github.com/*your github username*/GoogleBillboard
 // 9. If you have extra time you may want to see if you can solve the second puzzle. You will need more digits than the constant in the sample code has
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "GoogleBillboard" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
